31a1f9edec1df17102761fd1f15aed9f12ebdbb2
王 偉澔 <drxk41012@gmail.com>
2018-04-23 18:06:31 -0400

    w
    w

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var wifi = require('node-wifi');
+var parsedNetworks;
+var cors = require('cors');
+app.use(cors())
+
+wifi.init({
+	iface :null
+});
+wifi.scan(function(err, networks){
+	if(err){
+	console.log(err);
+}else{
+	console.log(networks);
+	parsedNetworks = [];
+	networks.forEach(function(e){
+		parsedNetworks.push({'ssid': e.ssid, 'signal_level':e.signal_level, 'mac':e.mac, 'security':e.security})
+	})
+	console.log(parsedNetworks);
+}
+});
+
+app.get('/getwifis', function (req, res) {
+   res.json(parsedNetworks);
+   // res.render('pagetest.html', {pn: parsedNetworks});
+})
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
+
