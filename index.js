64b98bd8fe2d9ebce801d33bd373e60884ffb454
王 偉澔 <drxk41012@gmail.com>
2018-04-23 18:39:11 -0400

    we
    ee

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var app = express();
 var wifi = require('node-wifi');
 var parsedNetworks;
 var cors = require('cors');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 app.use(cors())
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 wifi.init({
 	iface :null
 });
